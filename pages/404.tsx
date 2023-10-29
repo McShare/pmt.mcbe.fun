f796d6c6267e6036b39b7f0f179469e7cdf31f3b
SuperYYT <1209897320@qq.com>
2023-10-29 19:57:24 +0800

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 export default class Custom404 extends Component {
   render = () => (
     <Layout title={null} showNav={false}>
-      <h1>404 - Page Not Found</h1>
+      <h1>404 - 页面不见了！</h1>
     </Layout>
   );
 }
